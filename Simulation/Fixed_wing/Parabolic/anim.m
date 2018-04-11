b21aff90b4abcba021cd391c0d2ed0e2045df6b7
Jens Ludvik Grytnes Joberg <jljoberg@ntnu.no>
2018-04-11 16:03:31 +0200

    Day 13: Working parabolic randevouz manouver

--- Simulation/Fixed_wing/Parabolic/anim.m
+++ Simulation/Fixed_wing/Parabolic/anim.m
@@ -1,9 +1,9 @@
-
 % REMEMBER: Time is not linear!! Fix somehow
 % Fixed temporarily
 close all;
-dec = 4;    % Reduce samples
+dec = 8;    % Reduce samples
 
+%% Load data
 p = pos.Data;
 p(:,2) = - p(:,2);
 t = pos.Time;
@@ -12,9 +12,11 @@ th = theta.Data;
 p_t = target.Data;
 p_t(:,2) = -p_t(:,2);
 
+p_q = pos_quad.Data;
+p_q(:,2) = -p_q(:,2);
 
+%%
 figure; hold on;
-
 
 h_p =  animatedline();
 h_t = animatedline(); h_t.Color='g';
@@ -24,7 +26,19 @@ for i = 1:dec:size(t,1)
     addpoints(h_t, p_t(i,1), p_t(i,2));
     h1 = plot(p(i,1),p(i,2), 'b^');
     h2 = plot(p_t(i,1),p_t(i,2), 'rs');
-    axis([-5 50 -5 50])
-    pause(0.002); delete([h1 h2])
+    h3 = plot(p_q(i,1),p_q(i,2), 'go');
+    axis([-10 30 -10 30])
+    pause(0.00001); delete([h1 h2 h3])
     
 end
+
+%% Quad vs target error 
+error = zeros(size(p_q,1),1);
+for i =1:size(error,1)
+    error(i) = norm(p_q(i,:)-p_t(i,:));
+end
+figure; hold on
+plot(t,error)
+
+    
+
